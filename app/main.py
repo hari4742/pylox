8b266f31c20e04860bfb4039cc3c84acf361f800
HariKiran <harikiran.kante@gmail.com>
2025-01-20 01:15:09 +0530

    corrected error message

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from lox.lox import Lox
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
+    # print("Logs from your program will appear here!", file=sys.stderr)
 
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
