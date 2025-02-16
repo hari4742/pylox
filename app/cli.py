c9f4b2e7a2594d8cd77a9c91a223728c789e03b6
HariKiran <harikiran.kante@gmail.com>
2025-02-16 15:06:57 +0530

    added example scripts

--- app/cli.py
+++ app/cli.py
@@ -3,9 +3,6 @@ from lox.lox import Lox
 
 
 def cli():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
     if len(sys.argv) < 3:
         print("Usage: ./program <command> <filename>", file=sys.stderr)
         exit(1)
